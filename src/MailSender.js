1359908daf98a423939817e7e9c22c3469928530
M Akbar Maulana <makbarm.id@gmail.com>
2023-07-11 15:38:07 +0700

    refactor

--- src/MailSender.js
+++ src/MailSender.js
@@ -3,18 +3,18 @@ const nodemailer = require('nodemailer');
 class MailSender {
   constructor() {
     this._transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
       auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
+        user: process.env.MAIL_ADDRESS,
+        pass: process.env.MAIL_PASSWORD,
       },
     });
   }
 
   sendEmail(targetEmail, content) {
     const message = {
-      from: 'Open Music Apps',
+      from: 'Open Music <no-reply@openmusic.io>',
       to: targetEmail,
       subject: 'Export Playlist',
       text: 'Attached is the result of the export of records',
--- src/consumer.js
+++ src/consumer.js
@@ -13,9 +13,7 @@ const init = async () => {
   const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
   const channel = await connection.createChannel();
 
-  await channel.assertQueue('export:playlist', {
-    durable: true,
-  });
+  await channel.assertQueue('export:playlist', { durable: true });
 
   channel.consume('export:playlist', listener.listen, { noAck: true });
 };
